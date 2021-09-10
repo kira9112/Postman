18fa0243ce6d835f777a5b3fac924a94ef13ab7c
Kira9112 <kira91_911@mail.ru>
2021-09-10 23:41:57 +0300

    Test passed

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -13,7 +13,7 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Hello world"));
+                .body("data", equalTo("Hello world!"));
         ;
     }
 }
